7f5ca4d1d33f2063873b0dca50e7d8b967f2aed9
hhb <2636003896@qq.com>
2022-11-24 19:54:36 +0800

    master v1.0 NoChange

---
+++ src/main/java/com/example/gitdemo/test.java
@@ -0,0 +1,22 @@
+package com.example.gitdemo;
+
+import java.time.LocalDateTime;
+
+/**
+ * DATE:2022/11/24  15:19
+ */
+public class test {
+    public static void main(String[] args) {
+        String version = "v1.0";
+        String a = "master";
+
+    }
+
+    public void game(){
+        String version = "";
+    }
+
+    public void music(){
+        String version = "";
+    }
+}
---
+++ src/main/java/com/example/gitdemo/test2.java
@@ -0,0 +1,10 @@
+package com.example.gitdemo;
+
+/**
+ * DATE:2022/11/24  16:13
+ */
+public class test2 {
+  public void say(){
+    System.out.println("hello");
+  }
+}
